c5917dd0056e6b262b45ab0ab1d615f94dd35e0d
nuriansyah <nuriansyah09@gmail.com>
2021-01-01 10:14:52 +0700

    ## Feature Link to Resto ##

--- app/Views/hello/world.php
+++ app/Views/hello/world.php
@@ -17,6 +17,7 @@ $thumb = $json['featured_image'];
 $name = $json['name'];
 $timings = $json['timings'];
 $phone = $json['phone_numbers'];
+$url = $json['url'];
 
 $c = getUrl("https://developers.zomato.com/api/v2.1/restaurant?res_id=18477915&apikey=849d8360846e72f6e210e1dad475fbfa");
 
@@ -25,6 +26,7 @@ $thumbA = $c['featured_image'];
 $nameA = $c['name'];
 $timingsA = $c['timings'];
 $phoneA = $c['phone_numbers'];
+$urlA = $c['url'];
 
 $a = getUrl("https://developers.zomato.com/api/v2.1/restaurant?res_id=18619623&apikey=849d8360846e72f6e210e1dad475fbfa");
 
@@ -33,6 +35,7 @@ $thumbB = $a['featured_image'];
 $nameB = $a['name'];
 $timingsB = $a['timings'];
 $phoneB = $a['phone_numbers'];
+$urlB = $a['url'];
 
 
 
@@ -68,6 +71,7 @@ $phoneB = $a['phone_numbers'];
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
                             </svg> <?= $phone; ?></p>
+                        <a href="<?= $url; ?>" class="btn btn-primary">Go to Restaurant</a>
                     </div>
                 </div>
             </div>
@@ -88,6 +92,7 @@ $phoneB = $a['phone_numbers'];
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
                             </svg> <?= $phoneA; ?></p>
+                        <a href="<?= $urlA; ?>" class="btn btn-primary">Go to Restaurant</a>
                     </div>
                 </div>
             </div>
@@ -108,6 +113,7 @@ $phoneB = $a['phone_numbers'];
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
                             </svg> <?= $phoneB; ?></p>
+                        <a href="<?= $urlB; ?>" class="btn btn-primary">Go to Restaurant</a>
                     </div>
                 </div>
             </div>
